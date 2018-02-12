1ce1ef38b2e30160f74ecfc55e3c40d06af8ab18
Victor Caldas <caldas.victor@gmail.com>
2018-02-12 13:21:23 +0100

    start repo

---
+++ reader/reader/reader.py
@@ -0,0 +1,23 @@
+import os
+
+from reader.compressed import bzipped
+from reader.compressed import gzipped
+
+extension_map = {
+    '.bz2': bzipped.opener,
+    '.gz':  gzipped.opener
+}
+
+class Reader:
+    def __init__(self, filename):
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension, open)
+
+        self.filename = filename
+        self.f = opener(self.filename, 'rt')
+
+    def close(self) -> object:
+        self.f.close()
+
+    def read(self):
+        return self.f.read()
